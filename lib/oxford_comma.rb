092e418f29e2e072f2df49b34d61871893c19e0b
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-14 19:06:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     when 2
       array.join(" and ")
     when 3
-      array.join(", ") << ", and #{array[-1]}"
+      array.join[0..-1](", ") << ", and #{array[-1]}"
     else
       array.join(" ,")
 end
